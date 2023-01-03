67c081a0fd753d052e6e0f3f4dc4c78b6e0ad1f3
Evan BOISSONNOT <evan@boissonnot.fr>
2023-01-03 17:42:11 +0100

    feat: end of proxy

---
+++ Design patterns/DesignPatterns/JeuProjetFinal/Proxy/ApiPlanete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace JeuProjetFinal.Proxy
+{
+    internal class ApiPlanete 
+    {
+        public async Task<IEnumerable<Planete>> GetList()
+        {
+            using HttpClient client = new();
+
+            await using Stream stream = await client.GetStreamAsync("https://swapi.dev/api/planets");
+            return await JsonSerializer.DeserializeAsync<List<Planete>>(stream);
+        }
+    }
+}
---
+++ Design patterns/DesignPatterns/JeuProjetFinal/Proxy/CachePlaneteProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuProjetFinal.Proxy
+{
+    internal class CachePlaneteProxy : IPlaneteProxy
+    {
+        public IEnumerable<Planete>? GetList()
+        {
+            if (this.Planetes == null)
+            {
+                var proxy = new ApiPlanete();
+                this.Planetes = proxy.GetList().Result;
+            }
+
+            return this.Planetes;
+        }
+
+        public IEnumerable<Planete>? Planetes { get; private set; }
+    }
+}
